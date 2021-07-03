b5af49e6ae3f2acd823db69b238f735ad598a293
LANOEric <86877791+LANOEric@users.noreply.github.com>
2021-07-03 22:38:03 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// src/App.js
+
+import Posts from "./components/Posts";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>React Posts Sharer, please</h1>
+      </header>
+      <Posts />      
+    </div>
+  );
+}
+
+export default App;
